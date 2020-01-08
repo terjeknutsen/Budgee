b756a3e83413f5e3cced4ccbc371f392e57aa9df
terje knutsen <terje.knutsen@protonmail.com>
2020-01-08 22:50:15 +0100

    Added page and viewmodel

---
+++ Budgee.Mobile/Budgee.Mobile/DailyBudgets/DailyBudgetPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Budgee.Framework;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Budgee.Mobile.DailyBudgets
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DailyBudgetPage : ContentPage
+    {
+        public DailyBudgetPage(IApplicationService applicationService)
+        {
+            InitializeComponent();
+            BindingContext = new DailyBudgetViewModel(applicationService);
+        }
+    }
+}
